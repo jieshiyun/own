13656d2074ef718d86894fed88e446a8a331974b
jieshiyun <jieshiyun@gmail.com>
2015-11-15 01:48:04 +0800

    3

--- model/DBConnect.class.php
+++ model/DBConnect.class.php
@@ -6,7 +6,7 @@ class DBConnect{
 	protected $conn;
 
 	public function __construct(){
-		$this->conn = mysqli_connect((SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS) or die ('can not connect database');
+		$this->conn = mysqli_connect(SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS) or die ('can not connect database');
 		if($this->conn)
 			{
 			    mysql_select_db($this->conn，SAE_MYSQL_DB);
