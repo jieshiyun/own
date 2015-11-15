8932cfcf75eafa3aa1f17678561a99ada2d57423
jieshiyun <jieshiyun@gmail.com>
2015-11-15 13:35:01 +0800

    4

--- model/DBConnect.class.php
+++ model/DBConnect.class.php
@@ -9,7 +9,7 @@ class DBConnect{
 		$this->conn = mysqli_connect(SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS) or die ('can not connect database');
 		if($this->conn)
 			{
-			    mysqli_select_db($this->conn，SAE_MYSQL_DB);
+			    mysqli_select_db($this->conn,SAE_MYSQL_DB);
 			}
 		mysqli_query($this->conn,'set names utf8');
 	}	
