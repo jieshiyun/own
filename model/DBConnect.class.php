640d7e758bc4cb889ff1ef94e0d54a46e00610a5
jieshiyun <jieshiyun@gmail.com>
2015-11-15 23:52:00 +0800

    Dd

--- model/DBConnect.class.php
+++ model/DBConnect.class.php
@@ -6,7 +6,7 @@ class DBConnect{
 		$this->conn = mysqli_connect($GLOBAL_DB_CONF['host'].':'.$GLOBAL_DB_CONF['port'],$GLOBAL_DB_CONF['user'],$GLOBAL_DB_CONF['password']) or die ('can not connect database');
 		if($this->conn)
 			{
-			    mysqli_select_db($this->conn,SAE_MYSQL_DB);
+			    mysqli_select_db($this->conn,$GLOBAL_DB_CONF['dbname']);
 			}
 		mysqli_query($this->conn,'set names utf8');
 	}	
